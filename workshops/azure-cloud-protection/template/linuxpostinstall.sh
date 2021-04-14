02f18bbdf95e3276a32f30fb1413c22fee81c8de
David Okeyode <asegunlolu@hotmail.com>
2021-04-14 22:53:54 +0100

    modified template output

--- workshops/azure-cloud-protection/template/linuxpostinstall.sh
+++ workshops/azure-cloud-protection/template/linuxpostinstall.sh
@@ -10,5 +10,7 @@ sh get-docker.sh
 
 # Install Docker Compose
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-
 chmod +x /usr/local/bin/docker-compose
+
+# Install kubectl
+az aks install-cli
